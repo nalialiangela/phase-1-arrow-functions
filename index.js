4b5b6422a53da8b474cd57bab864cc293c2ed095
naliali <nalialiangela365@gmail.com>
2024-03-26 13:33:06 +0300

    arrow functions

---
+++ index.js
@@ -0,0 +1,19 @@
+const divide = function(array, divisor)
+{
+    return array.map(item => item / divisor);
+
+}
+
+const resul =divide([2000], 100);
+console.log(resul);
+
+
+const square = (number) => number * number;
+const num = 5;
+const squaredNum = square(num);
+console.log(squaredNum);
+
+const add = (a, b) => a + b;
+const result = add(5,3);
+
+console.log(result);
